f4f6a5aee89798021da24b996edeec7c37457306
TheBogdichHD <nottoowellconstructed@gmail.com>
2022-10-31 15:06:55 +0300

    Player controll update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour, IController
     private Rigidbody2D rb;
     private Transform groundCheck;
     private LayerMask groundLayer;
+
+    private float coyeteTime = 0.2f;
+    private float coyeteTimeCounter;
+
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
     Vector3 MovementVector { get; }
     private float horizontal;
     public float Speed { get; set; }
@@ -14,6 +20,7 @@ public class PlayerController : MonoBehaviour, IController
 
     private bool isFacingRight = true;
 
+    
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
@@ -25,15 +32,38 @@ public class PlayerController : MonoBehaviour, IController
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (IsGrounded())
+        {
+            coyeteTimeCounter = coyeteTime;
+        }
+        else
+        {
+            coyeteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0 && coyeteTimeCounter > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
+
+            jumpBufferCounter = 0;
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+
+            coyeteTimeCounter = 0;
         }
+
         Flip();
         rb.velocity = new Vector2(horizontal * Speed, rb.velocity.y);
     }
