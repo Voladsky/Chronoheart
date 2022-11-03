ba4433fd5d09b32f6d47f3de65ee291f2a7cf2e6
TheBogdichHD <nottoowellconstructed@gmail.com>
2022-11-03 23:04:48 +0300

    Better controls implemented
    Need further configurations

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 1f, -10f);
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
