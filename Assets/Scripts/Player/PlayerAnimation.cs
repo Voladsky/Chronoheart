0f34165de09911a07b978aa1a52b0f418c955df9
TheBogdichHD <nottoowellconstructed@gmail.com>
2022-11-25 22:01:20 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/Voladsky/Chronoheart""
    This reverts commit f41b7900fe5936b236c231e0868bde871ca8fc74.

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -20,6 +20,7 @@ public class PlayerAnimation : MonoBehaviour
     const string PLAYER_IDLE = "PlayerIdle";
     const string PLAYER_RUN = "PlayerRun";
     const string PLAYER_JUMP = "PlayerJump";
+    const string PLAYER_FALL = "PlayerBeginToFall";
     const string PLAYER_ATTACK = "PlayerAttack";
     const string PLAYER_AIR_ATTACK = "PlayerAirAttack";
 
@@ -53,12 +54,29 @@ public class PlayerAnimation : MonoBehaviour
                 ChangeAnimationState(PLAYER_IDLE);
             }
         }
-        /*
-        if (player.LastPressedJumpTime > 0 && player.LastOnGroundTime < 0)
-        {
-            ChangeAnimationState(PLAYER_JUMP);
-        }
 
+
+        if (player.LastOnGroundTime <= 0)
+        {           
+            if (player._isJumpFalling)
+            {
+                ChangeAnimationState(PLAYER_FALL);
+            }
+            else
+            {
+                if (player.IsJumping)
+                {
+                    ChangeAnimationState(PLAYER_JUMP);
+                }
+                else
+                {
+                    ChangeAnimationState(PLAYER_FALL);
+                }
+            }
+        }
+        
+        
+        /*
         //attack
         if (isAttackPressed)
         {
