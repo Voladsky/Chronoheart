73a5c75676fb38d68027c6f677698734a2acb3d2
TheBogdichHD <nottoowellconstructed@gmail.com>
2022-12-01 21:04:30 +0300

    Death animations added

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
+    [SerializeField] float deathDelay = 2.0f;
+
     private void OnDisable()
+    {
+        Invoke("LoadLevel", deathDelay);
+    }
+
+    private void LoadLevel()
     {
         LevelLoader.instance.LoadSameLevel();
     }
