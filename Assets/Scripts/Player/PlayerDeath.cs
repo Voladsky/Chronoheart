eb4ea0935ece282d41154bdc0326a679c192eee5
TheBogdichHD <nottoowellconstructed@gmail.com>
2022-12-02 17:41:09 +0300

    Player animations fixes

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -7,7 +7,8 @@ public class PlayerDeath : MonoBehaviour
     [SerializeField] float deathDelay = 2.0f;
 
     private void OnDisable()
-    {      
+    {    
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         Invoke("LoadLevel", deathDelay);
     }
 
