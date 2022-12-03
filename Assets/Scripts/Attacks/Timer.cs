89f42787d434b990ab6dd8167fd370961fa4210e
TheBogdichHD <nottoowellconstructed@gmail.com>
2022-12-03 10:17:08 +0300

    Tick image fix

--- Assets/Scripts/Attacks/Timer.cs
+++ Assets/Scripts/Attacks/Timer.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public bool CurTick { get; set; } = true;
     public float BPM_Timer { get; set; } = 0.46875f;
-    Color[] clrs;
+    Color[] clrs = new Color[] { Color.black, Color.yellow };
 
+    [SerializeField] Image timerImage;
     void Start()
     {
-        clrs = new Color[] { Color.black, Color.yellow };
         StartCoroutine(Time_Dec());
     }
     private IEnumerator Time_Dec()
     {
         int i = 0;
         while (true) {
-            gameObject.GetComponent<SpriteRenderer>().color = clrs[i];
+            timerImage.color = clrs[i];
             CurTick = !CurTick;
             i = (i + 1) % 2;
             yield return new WaitForSeconds(BPM_Timer);
