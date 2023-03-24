f08e4f2fb94e07c2e58fd8bc89b237baa3c7d8ba
Владислав Крылов <krylov@sfedu.ru>
2023-03-24 15:13:33 +0300

    Experimenting with combo system
    Now a combo must be perfomed all in one tick. One can also perform combo whilst holding movement buttons.

--- Assets/Scripts/Attacks/ComboChecker.cs
+++ Assets/Scripts/Attacks/ComboChecker.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 public class ComboChecker : MonoBehaviour
 {
     [SerializeField] PlayerAttack playerAttack;
     [SerializeField] Timer timer;
     [SerializeField] TextMeshProUGUI comboText;
+    [SerializeField] float comboCooldown;
     HashSet<string> combos;
     string curCombo;
     bool registered;
@@ -19,63 +21,92 @@ public class ComboChecker : MonoBehaviour
         combos = new HashSet<string> { "00", "01", "450", "550", "320", "230" };
         lastButtonInTick = ATK_BUTTONS.NONE;
         registered = false;
-        StartCoroutine(MyFixedUpd());
+        //StartCoroutine(MyFixedUpd());
     }
-    IEnumerator MyFixedUpd()
+
+    private void Update()
     {
-        while (true)
+        comboCooldown -= Time.deltaTime;
+        if (timer.CurTick)
         {
-            if (timer.CurTick)
+            var btn = ParseKey();
+            if (btn != ATK_BUTTONS.NONE)
             {
-                registered = false;
-                var btn = ParseKey();
-                if (btn != ATK_BUTTONS.NONE)
+                lastButtonInTick = btn;
+                curCombo += (int)(btn - 1);
+                Debug.Log(curCombo);
+                if (combos.Any(x => curCombo.Contains(x)) && comboCooldown < 0)
                 {
-                    lastButtonInTick = btn;
-                    curCombo += (int)(btn - 1);
-                    Debug.Log(curCombo);
-                    if (combos.Contains(curCombo))
-                    {
-                        StartCoroutine(ShowText());
-                        playerAttack.Attack(100, true);
-                        curCombo = "";
-                    }
-                    yield return new WaitUntil(() => !timer.CurTick);
-                }
-                else yield return new WaitUntil(() => timer.CurTick);
-            }
-            else
-            {
-                var btn = ParseKey();
-                if (btn != ATK_BUTTONS.NONE)
-                {
+                    StartCoroutine(ShowText());
+                    PerformCombo(curCombo);
+                    playerAttack.Attack(100, true);
                     curCombo = "";
-                    yield return new WaitUntil(() => timer.CurTick);
-                }
-                if (lastButtonInTick == ATK_BUTTONS.NONE && !registered)
-                {
-                    curCombo = "";
-                    yield return new WaitUntil(() => timer.CurTick);
-                }
-                else
-                {
-                    registered = true;
-                    lastButtonInTick = ATK_BUTTONS.NONE;
-                    yield return new WaitUntil(() => !timer.CurTick);
                 }
             }
         }
+        else
+        {
+            curCombo = "";
+        }
     }
 
+    /*    IEnumerator MyFixedUpd()
+        {
+            while (true)
+            {
+                if (timer.CurTick)
+                {
+                    registered = false;
+                    var btn = ParseKey();
+                    //Debug.Log(btn);
+                    if (btn != ATK_BUTTONS.NONE)
+                    {
+                        lastButtonInTick = btn;
+                        curCombo += (int)(btn - 1);
+                        Debug.Log(curCombo);
+                        if (combos.Any(x => curCombo.Contains(x)))
+                        {
+                            StartCoroutine(ShowText());
+                            PerformCombo(curCombo);
+                            playerAttack.Attack(100, true);
+                            curCombo = "";
+                        }
+                        yield return new WaitUntil(() => !timer.CurTick);
+                    }
+                    else yield return new WaitUntil(() => timer.CurTick);
+                }
+                else
+                {
+                    var btn = ParseKey();
+                    if (btn != ATK_BUTTONS.NONE)
+                    {
+                        curCombo = "";
+                        yield return new WaitUntil(() => timer.CurTick);
+                    }
+                    if (lastButtonInTick == ATK_BUTTONS.NONE && !registered)
+                    {
+                        curCombo = "";
+                        yield return new WaitUntil(() => timer.CurTick);
+                    }
+                    else
+                    {
+                        registered = true;
+                        lastButtonInTick = ATK_BUTTONS.NONE;
+                        yield return new WaitUntil(() => !timer.CurTick);
+                    }
+                }
+            }
+        }
+    */
     ATK_BUTTONS ParseKey()
     {
         if (Input.GetMouseButtonDown(0)) return ATK_BUTTONS.LEFT_CLICK;
         if (Input.GetMouseButtonDown(1)) return ATK_BUTTONS.RIGHT_CLICK;
-        if (Input.GetAxis("Vertical") == 1) return ATK_BUTTONS.MOVE_UP;
-        if (Input.GetAxis("Horizontal") == -1) return ATK_BUTTONS.MOVE_LEFT;
-        if (Input.GetAxis("Vertical") == -1) return ATK_BUTTONS.MOVE_DOWN;
-        if (Input.GetAxis("Horizontal") == 1) return ATK_BUTTONS.MOVE_RIGHT;
-        else return ATK_BUTTONS.NONE;
+        if (Input.GetKeyDown("space")) return ATK_BUTTONS.MOVE_UP;
+        if (Input.GetKeyDown("s")) return ATK_BUTTONS.MOVE_DOWN; 
+        if (Input.GetKeyDown("a")) return ATK_BUTTONS.MOVE_LEFT;
+        if (Input.GetKeyDown("d")) return ATK_BUTTONS.MOVE_RIGHT;
+        return ATK_BUTTONS.NONE;
     }
 
     IEnumerator ShowText()
@@ -87,5 +118,21 @@ public class ComboChecker : MonoBehaviour
         yield return new WaitForSeconds(1);
 
         comboText.faceColor = new Color32(255, 255, 255, 0);
+    }
+
+    void PerformCombo(string cmb)
+    {
+        switch (cmb)
+        {
+            case "00":
+                Debug.Log("Heavy attack combo!");
+                break;
+            case "01":
+                Debug.Log("Long range combo");
+                break;
+            case "450":
+                Debug.Log("Combo with moves!");
+                break;
+        }
     }
 }
