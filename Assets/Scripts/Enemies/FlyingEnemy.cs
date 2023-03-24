79596486dfad217f2cb5296ab130a07ee715fa03
LordPelmeha <sashasofolov@gmail.com>
2023-03-24 21:43:01 +0300

    Сontinue to work

--- Assets/Scripts/Enemies/FlyingEnemy.cs
+++ Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -22,19 +22,74 @@ public class FlyingEnemy : MonoBehaviour
     //References
     private Animator anim;
     private Health playerHealth;
-    //private FlyingEnemyBehaviour enemyPatrol; добавить позже
+    private EnemyBehaviour enemyPatrol;
 
     [SerializeField] private UnityEvent onEnemyDie;
 
     [SerializeField] AudioClip attackSound;
-    void Start()
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyBehaviour>();
     }
 
-    
-    void Update()
+    private void Update()
     {
-        
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight?
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                SoundManager.Instance.PlaySoundWithRandomValues(attackSound);
+                cooldownTimer = 0;
+                anim.SetTrigger("meleeAttack");
+            }
+        }
+
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerInSight();
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
+        return hit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && contactDamage != 0)
+        {
+            collision.GetComponent<Health>().TakeDamage(contactDamage);
+        }
+    }
+
+    private void OnDisable()
+    {
+        contactDamage = 0;
+        Destroy(GetComponent<Rigidbody2D>());
+        onEnemyDie.Invoke();
+        GetComponent<SpriteRenderer>().sortingLayerName = "Other";
     }
 }
