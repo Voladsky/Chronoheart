19bf35276572abb9835e7bc734a0b348e3d06aec
TheBogdichHD <nottoowellconstructed@gmail.com>
2023-03-26 14:00:41 +0300

    Boss 1 phase added

--- Assets/Scripts/Enemies/Boss1/BossBall.cs
+++ Assets/Scripts/Enemies/Boss1/BossBall.cs
@@ -1,19 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BossBall : MonoBehaviour
 {
+    
+    [SerializeField] Transform player;
+
+    [SerializeField] bool isFlipped = false;
+
+    [SerializeField] int contactDamage = 5;
+    [SerializeField] int attackDamage = 20;
+
+    [SerializeField] Vector3 attackOffset;
+    [SerializeField] float attackRange = 1f;
+    [SerializeField] LayerMask attackMask;
+
+    [SerializeField] private UnityEvent onEnemyDie;
+    [SerializeField] AudioClip attackSound;
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {         
+            colInfo.GetComponent<Health>().TakeDamage(attackDamage);
+        }
+
+        SoundManager.Instance.PlaySoundWithRandomValues(attackSound);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Gizmos.DrawWireSphere(pos, attackRange);
+    }
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
     private void OnDisable()
     {
-        //contactDamage = 0;
+        contactDamage = 0;
         Rigidbody2D rb;
         if (TryGetComponent<Rigidbody2D>(out rb))
         {
             rb.simulated = false;
         }
 
-        //onEnemyDie.Invoke();
+        onEnemyDie.Invoke();
         GetComponent<SpriteRenderer>().sortingLayerName = "Other";
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 }
