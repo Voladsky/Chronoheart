e45275242c8d22f56bb6be50bbfc4ce3fac00a10
Владислав Крылов <krylov@sfedu.ru>
2023-03-27 17:43:01 +0300

    Added long range combo + tweaks
    Settings in pause menu in boss added

--- Assets/Scripts/Attacks/ComboChecker.cs
+++ Assets/Scripts/Attacks/ComboChecker.cs
@@ -9,16 +9,20 @@ public class ComboChecker : MonoBehaviour
     [SerializeField] Timer timer;
     [SerializeField] TextMeshProUGUI comboText;
     [SerializeField] float comboCooldown;
+    [SerializeField] private RangeWeapon weapon;
     HashSet<string> combos;
     string curCombo;
     bool registered;
+
+    private float cooldown;
+    private float attack_timer;
 
     enum ATK_BUTTONS { NONE, LEFT_CLICK, RIGHT_CLICK, MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT }
     ATK_BUTTONS lastButtonInTick;
     void Start()
     {
         curCombo = "";
-        combos = new HashSet<string> { "00", "01", "450", "550", "320", "230" };
+        combos = new HashSet<string> { "00", "01", "450", "540", "440", "550", "320", "230" };
         lastButtonInTick = ATK_BUTTONS.NONE;
         registered = false;
         //StartCoroutine(MyFixedUpd());
@@ -37,7 +41,6 @@ public class ComboChecker : MonoBehaviour
                 Debug.Log(curCombo);
                 if (combos.Any(x => curCombo.Contains(x)) && comboCooldown < 0)
                 {
-                    StartCoroutine(ShowText());
                     PerformCombo(curCombo);
                     curCombo = "";
                 }
@@ -48,55 +51,6 @@ public class ComboChecker : MonoBehaviour
             curCombo = "";
         }
     }
-
-    /*    IEnumerator MyFixedUpd()
-        {
-            while (true)
-            {
-                if (timer.CurTick)
-                {
-                    registered = false;
-                    var btn = ParseKey();
-                    //Debug.Log(btn);
-                    if (btn != ATK_BUTTONS.NONE)
-                    {
-                        lastButtonInTick = btn;
-                        curCombo += (int)(btn - 1);
-                        Debug.Log(curCombo);
-                        if (combos.Any(x => curCombo.Contains(x)))
-                        {
-                            StartCoroutine(ShowText());
-                            PerformCombo(curCombo);
-                            playerAttack.Attack(100, true);
-                            curCombo = "";
-                        }
-                        yield return new WaitUntil(() => !timer.CurTick);
-                    }
-                    else yield return new WaitUntil(() => timer.CurTick);
-                }
-                else
-                {
-                    var btn = ParseKey();
-                    if (btn != ATK_BUTTONS.NONE)
-                    {
-                        curCombo = "";
-                        yield return new WaitUntil(() => timer.CurTick);
-                    }
-                    if (lastButtonInTick == ATK_BUTTONS.NONE && !registered)
-                    {
-                        curCombo = "";
-                        yield return new WaitUntil(() => timer.CurTick);
-                    }
-                    else
-                    {
-                        registered = true;
-                        lastButtonInTick = ATK_BUTTONS.NONE;
-                        yield return new WaitUntil(() => !timer.CurTick);
-                    }
-                }
-            }
-        }
-    */
     ATK_BUTTONS ParseKey()
     {
         if (Input.GetMouseButtonDown(0)) return ATK_BUTTONS.LEFT_CLICK;
@@ -108,9 +62,9 @@ public class ComboChecker : MonoBehaviour
         return ATK_BUTTONS.NONE;
     }
 
-    IEnumerator ShowText()
+    IEnumerator ShowText(string text)
     {
-        comboText.text = $"CCCOMBO!!!";
+        comboText.text = text;
 
         comboText.faceColor = new Color32(255, 255, 255, 255);
 
@@ -124,15 +78,20 @@ public class ComboChecker : MonoBehaviour
         switch (cmb)
         {
             case "00":
-                Debug.Log("Heavy attack combo!");
+                StartCoroutine(ShowText("Heavy attack!"));
                 playerAttack.Attack(100, true);
                 break;
             case "01":
-                Debug.Log("Long range combo");
+                StartCoroutine(ShowText("Long range attack!"));
+                weapon.ComboAttack(GetComponent<Player>().transform.localScale.x);
                 break;
             case "450":
-                Debug.Log("Combo with moves!");
+                StartCoroutine(ShowText("Movin!"));
                 break;
+            case "540":
+                StartCoroutine(ShowText("Movin!"));
+                break;
+
         }
     }
 }
--- Assets/Scripts/Player/RangeWeapon.cs
+++ Assets/Scripts/Player/RangeWeapon.cs
@@ -7,6 +7,7 @@ using UnityEngine.Tilemaps;
 public class RangeWeapon : MonoBehaviour
 {
     [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject combo_projectile;
     [SerializeField] private Transform range;
     public void Attack(float scale)
     {
@@ -15,6 +16,16 @@ public class RangeWeapon : MonoBehaviour
         if (collisions.Length != 1 || collisions.First().GetComponent<TilemapCollider2D>() == null)
         {
             Instantiate(projectile, range.position, range.rotation);
+        }
+    }
+
+    public void ComboAttack(float scale)
+    {
+        combo_projectile.transform.localScale = new Vector3(scale, 1, 1);
+        var collisions = Physics2D.OverlapCircleAll(range.position, combo_projectile.GetComponent<SpriteRenderer>().size.normalized.x);
+        if (collisions.Length != 1 || collisions.First().GetComponent<TilemapCollider2D>() == null)
+        {
+            Instantiate(combo_projectile, range.position, range.rotation);
         }
     }
 }
