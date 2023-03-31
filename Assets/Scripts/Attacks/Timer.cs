9c3c9be00f1cbee8a121620b95077d6c3555e20a
TheBogdichHD <nottoowellconstructed@gmail.com>
2023-03-31 20:25:41 +0300

    Metronom added

--- Assets/Scripts/Attacks/Timer.cs
+++ Assets/Scripts/Attacks/Timer.cs
@@ -6,27 +6,26 @@ public class Timer : MonoBehaviour
 {
     public bool CurTick { get; set; } = true;
     public float BPM_Timer;
-    Color[] clrs;
+    [SerializeField] Sprite[] images;
 
     [SerializeField] Image timerImage;
     [SerializeField] int startState = 0;
     [SerializeField] float offSet;
     void Start()
     {
-        clrs = new Color[] { Color.black, Color.yellow };
         StartCoroutine(Time_Dec());
     }
     private IEnumerator Time_Dec()
     {
         int i = startState;
-        timerImage.color = clrs[i];
+        timerImage.sprite = images[i];
         CurTick = startState != 0;
         i = (i + 1) % 2;
         yield return new WaitForSeconds(offSet);
 
         while (true) 
         {           
-            timerImage.color = clrs[i];
+            timerImage.sprite = images[i];
             CurTick = !CurTick;
             i = (i + 1) % 2;
             yield return new WaitForSeconds(BPM_Timer);
